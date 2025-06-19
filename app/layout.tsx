7b37ee9170183d092139dee7579e2fe6c6b4e4bb
Vaibhav Sharma <vaibhavhimanshusharma@gmail.com>
2025-06-19 11:34:53 +0530

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
 import "./global.css";
 
 export const metadata = {
-    title: "F1GPT",
-    description: "F1GPT - Your AI companion for Formula 1"
-}
+  title: "F1GPT",
+  description: "F1GPT - Your AI companion for Formula 1",
+};
+
 const RootLayout = ({ children }) => {
-    return (
-        <html lang="en">
-            <head>
-                <link rel="icon" href="/favicon.ico" />
-            </head>
-            <body className="bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
-                {children}
-            </body>
-        </html>
-    );
-}
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
+      <body className="bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+        {children}
+      </body>
+    </html>
+  );
+};
 
 export default RootLayout;
-
